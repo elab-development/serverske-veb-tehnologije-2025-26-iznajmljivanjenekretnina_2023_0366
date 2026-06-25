f628526083f9b9295795c7b2f2a022f307b911ef
vukasinsimeunovic004-wq <vukasinsimeunovic004@gmail.com>
2026-06-25 12:54:56 +0200

    Category Controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,3 +10,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::apiResource('categories', CategoryController::class)->only([
+    'index',
+    'show',
+]);
+
+Route::apiResource('categories', CategoryController::class)->only([
+    'store',
+    'update',
+    'destroy',
+])->middleware('auth:sanctum');
