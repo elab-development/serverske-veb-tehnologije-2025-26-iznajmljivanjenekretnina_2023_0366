a64cbbae7b427a0147be46f766c176cc2b80ce71
anastasiadmitrovic <anastasia.dmitrovic@gmail.com>
2026-06-25 19:17:07 +0200

    ExternalData controller

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExternalDataController;
 use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/external/geocode', [ExternalDataController::class, 'geocode']);
+Route::get('/external/weather', [ExternalDataController::class, 'weather']);
 
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -30,6 +33,9 @@ Route::apiResource('properties', PropertyController::class)->only([
     'index',
     'show',
 ]);
+
+Route::get('/properties/{property}/location', [ExternalDataController::class, 'propertyLocation']);
+Route::get('/properties/{property}/weather', [ExternalDataController::class, 'propertyWeather']);
 
 Route::apiResource('properties', PropertyController::class)->only([
     'store',
